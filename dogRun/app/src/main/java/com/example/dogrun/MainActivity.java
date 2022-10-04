ba3e65c8da1f7129919b4076db58a5607ffbed53
unknown <zcen2@hawk.iit.edu>
2022-10-03 19:47:26 -0500

    New Front-End renders as well as updates to current

--- dogRun/app/src/main/java/com/example/dogrun/MainActivity.java
+++ dogRun/app/src/main/java/com/example/dogrun/MainActivity.java
@@ -21,21 +21,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        pin= findViewById(R.id.pin);
+        setContentView(R.layout.setup_screen);
+        pin = findViewById(R.id.pin);
     }
 
     public void submit(View v) {
         String pin_input = pin.getText().toString();
         int int_pin = parseInt(pin_input);
-        if(int_pin == correct_pin)
+        if (int_pin == correct_pin) {
             Toast.makeText(getApplicationContext(), "Success!",
 
                     Toast.LENGTH_LONG).show();
             setContentView(R.layout.option1);
-
         }
+        else{
+            Toast.makeText(getApplicationContext(), "Incorrect Pin",
 
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void phone_mode_click(View v) {
+
+        Toast.makeText(getApplicationContext(), "Phone Mode",
+
+                Toast.LENGTH_LONG).show();
+                setContentView(R.layout.phone_mode);
 
     }
+    public void complete_click(View v) {
+
+        Toast.makeText(getApplicationContext(), "Setup Complete",
+
+                Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+
+    }
+}
 
